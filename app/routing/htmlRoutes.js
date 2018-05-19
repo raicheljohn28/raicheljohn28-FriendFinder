d54d73ab3f460b69f70741c81cf4858ac5e80540
Raichel John <raicheljohn28@gmail.com>
2018-05-19 18:20:24 -0400

    updates

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,8 +10,8 @@ module.exports = function(app) {
         
     });
 
-    app.get("survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "survey.html"));
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 };
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "./app/public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text);
+// app.use(bodyParser.text);
 
 //Add the routes
 
